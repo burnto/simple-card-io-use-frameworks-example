11ae7225479fdc7b9594d8ec434d274ec9630511
Brent Fitzgerald <brent.fitzgerald@getbraintree.com>
2015-08-28 14:53:11 -0700

    Initial Commit

---
+++ simple-card-io/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  simple-card-io
+//
+//  Created by Fitzgerald, Brent on 8/28/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ simple-card-ioTests/simple_card_ioTests.m
@@ -0,0 +1,40 @@
+//
+//  simple_card_ioTests.m
+//  simple-card-ioTests
+//
+//  Created by Fitzgerald, Brent on 8/28/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface simple_card_ioTests : XCTestCase
+
+@end
+
+@implementation simple_card_ioTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
